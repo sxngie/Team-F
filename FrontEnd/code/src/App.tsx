4740d17425bf6ebfec03aa781a508ff2779ddca5
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2021-09-03 12:44:41 -0400

    Reverted font size to not use rem.

--- FrontEnd/code/src/App.tsx
+++ FrontEnd/code/src/App.tsx
@@ -1,9 +1,14 @@
 import './styles/app.sass';
 
+import Icon from 'components/Icon';
 import React from 'react';
 
 const App = () => {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<button className="button">Start your search</button>
+		</div>
+	);
 };
 
 export default App;
