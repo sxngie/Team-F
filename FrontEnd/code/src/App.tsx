d91738928c79f1f991bd8dd362a385f3dfb84360
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2021-09-03 12:49:36 -0400

    Removed test button from the main app.

--- FrontEnd/code/src/App.tsx
+++ FrontEnd/code/src/App.tsx
@@ -1,14 +1,9 @@
 import './styles/app.sass';
 
-import Icon from 'components/Icon';
 import React from 'react';
 
 const App = () => {
-	return (
-		<div className="app">
-			<button className="button">Start your search</button>
-		</div>
-	);
+	return <div className="app"></div>;
 };
 
 export default App;
