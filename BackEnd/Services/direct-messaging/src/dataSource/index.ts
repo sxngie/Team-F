bea3fe284aca0a431f083e4251ec5b973729afe5
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2021-10-19 14:53:01 -0400

    Made the database access internal to the datasource

--- BackEnd/Services/direct-messaging/src/dataSource/index.ts
+++ BackEnd/Services/direct-messaging/src/dataSource/index.ts
@@ -5,17 +5,25 @@ import { PrismaClient } from '@prisma/client';
 
 type C = Omit<ApolloContext, "dataSources">;
 
-export default class ServerApi extends DataSource<C> {
+export default class MsgApi extends DataSource<C> {
 	private context!: C;
 	private prisma: PrismaClient;
 
-	public constructor(prisma: PrismaClient) {
+	public constructor() {
 		super();
-		this.prisma = prisma;
+		this.prisma = new PrismaClient({ errorFormat: "minimal" });
 	}
 
 	public initialize(config: DataSourceConfig<C>) {
 		this.context = config.context;
+	}
+
+	/**
+	 * Disconnect from the database.\
+	 * *Should run this when this class is no longer in use.*
+	 */
+	public disconnect() {
+		this.prisma.$disconnect();
 	}
 
 	//TODO: create the data source methods to acces the data layer.
