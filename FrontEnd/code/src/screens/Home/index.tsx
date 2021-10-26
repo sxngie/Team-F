b9fa1526f09d2ed58a604252ce6a58737ed5fecb
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2021-10-26 00:57:30 -0400

    Found method to style element based on parent size

--- FrontEnd/code/src/screens/Home/index.tsx
+++ FrontEnd/code/src/screens/Home/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import Player from 'modules/Player';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { useResizeDetector } from 'react-resize-detector';
 
 import styles from './Home.module.sass';
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
-	return <main className={styles.main}></main>;
+	const compact = useRef(false);
+	const onResize = useCallback((width?: number) => {
+		if (!width) return;
+
+		if (!compact.current && width < 700) {
+			compact.current = true;
+		} else if (compact && width >= 700) {
+			compact.current = false;
+		}
+	}, []);
+
+	const { ref } = useResizeDetector({ onResize });
+
+	return (
+		<main className={styles.main} ref={ref}>
+			<Player compact={compact.current} />
+		</main>
+	);
 };
 
 export default Home;
