e5f288685a23c39a6d549510436e9528df78da67
Santos O. Martínez Braña <santos.martinez1@upr.edu>
2021-10-26 23:23:02 -0400

    Added some functionality to the card and created mount animation

--- FrontEnd/code/src/common/components/ProfileCard/index.tsx
+++ FrontEnd/code/src/common/components/ProfileCard/index.tsx
@@ -1,5 +1,6 @@
 import cn from 'classnames';
 import React, { useState } from 'react';
+import { animated, config, useTransition } from 'react-spring';
 
 import Avatars from '../Avatars';
 import Icon from '../Icon';
@@ -9,88 +10,117 @@ import useGetUser, { followShort, getMutualStr } from './util';
 // Component properties
 interface Props {
 	username: string;
+	visible?: boolean;
 }
 
-const ProfileCard: React.FC<Props> = ({ username }) => {
+const ProfileCard: React.FC<Props> = ({ username, visible }) => {
 	const data = useGetUser(username);
 	const [followed, setFollowed] = useState(data.isAdded);
+	const transitions = useTransition(visible, {
+		from: { opacity: 0, transform: "scale(0.8)" },
+		enter: { opacity: 1, transform: "scale(1)" },
+		leave: { opacity: 0, transform: "scale(0.8)" },
+		config: {
+			...config.gentle,
+			duration: 200,
+		},
+	});
 
 	const [error, setError] = useState({
 		avatar: false,
 		banner: false,
 	});
 
-	return (
-		<div className={styles.container}>
-			<div className={styles.top}>
-				<button
-					className={cn(styles.button, {
-						[styles.followed]: followed,
-					})}
-					onClick={() => setFollowed((v) => !v)}
-				>
-					<span>{followed ? "Following" : "Follow"}</span>
-				</button>
-				{error.avatar ? (
-					<span className={styles.avatar}>
-						<Icon name="user" size={32} />
-					</span>
-				) : (
-					<img
-						className={styles.avatar}
-						alt="Avatar"
-						onError={() => setError({ ...error, avatar: true })}
-						src={data.avatar}
-					/>
-				)}
-				{error.banner ? (
-					<span className={styles.banner}>
-						<Icon name="image" size={32} />
-					</span>
-				) : (
-					<img
-						className={styles.banner}
-						src={data.banner}
-						alt="Banner"
-						onError={() => setError({ ...error, banner: true })}
-					/>
-				)}
-			</div>
-			<div className={styles.bottom}>
-				<button className={styles.user}>
-					<div className={styles.nameWrapper}>
-						<p className={styles.name}>{data.name}</p>
-						{data.verified && (
-							<Icon name="verified" className={styles.verified} />
+	return transitions(
+		(style, item) =>
+			item && (
+				<animated.div className={styles.container} style={style}>
+					<div className={styles.top}>
+						<button
+							className={cn(styles.button, {
+								[styles.followed]: followed,
+							})}
+							onClick={() => setFollowed((v) => !v)}
+						>
+							<span>{followed ? "Following" : "Follow"}</span>
+						</button>
+						{error.avatar ? (
+							<span className={styles.avatar}>
+								<Icon name="user" size={32} />
+							</span>
+						) : (
+							<img
+								className={styles.avatar}
+								alt="Avatar"
+								onError={() =>
+									setError({ ...error, avatar: true })
+								}
+								src={data.avatar}
+							/>
+						)}
+						{error.banner ? (
+							<span className={styles.banner}>
+								<Icon name="image" size={32} />
+							</span>
+						) : (
+							<img
+								className={styles.banner}
+								src={data.banner}
+								alt="Banner"
+								onError={() =>
+									setError({ ...error, banner: true })
+								}
+							/>
 						)}
 					</div>
-					<p className={styles.username}>{`@${username}`}</p>
-				</button>
-				<p className={styles.description}>{data.description}</p>
+					<div className={styles.bottom}>
+						<button className={styles.user}>
+							<div className={styles.nameWrapper}>
+								<p className={styles.name}>{data.name}</p>
+								{data.verified && (
+									<Icon
+										name="verified"
+										className={styles.verified}
+									/>
+								)}
+							</div>
+							<p className={styles.username}>{`@${username}`}</p>
+						</button>
+						<p className={styles.description}>{data.description}</p>
 
-				<span className={styles.follows}>
-					<button className={styles.numbers}>
-						<span>{followShort(data.following)}</span>
-						<span className={styles.text}>Following</span>
-					</button>
+						<span className={styles.follows}>
+							<button className={styles.numbers}>
+								<span>{followShort(data.following)}</span>
+								<span className={styles.text}>Following</span>
+							</button>
 
-					<button className={styles.numbers}>
-						<span>{followShort(data.followers)}</span>
-						<span className={styles.text}>Followers</span>
-					</button>
-				</span>
-				<button className={styles.mutualWrapper}>
-					<Avatars
-						type={data.mutuals.top.length}
-						avatars={data.mutuals.top.map(({ avatar }) => avatar)}
-					/>
-					<p className={styles.mutuals}>
-						{getMutualStr(data.mutuals)}
-					</p>
-				</button>
-			</div>
-		</div>
+							<button className={styles.numbers}>
+								<span>{followShort(data.followers)}</span>
+								<span className={styles.text}>Followers</span>
+							</button>
+						</span>
+						<button className={styles.mutualWrapper}>
+							<Avatars
+								type={data.mutuals.top.length}
+								avatars={data.mutuals.top.map(
+									({ avatar }) => avatar
+								)}
+							/>
+							<p className={styles.mutuals}>
+								{getMutualStr(data.mutuals)}
+							</p>
+						</button>
+					</div>
+				</animated.div>
+			)
 	);
 };
 
-export default ProfileCard;
+/**
+ * Displays content in the center panel.
+ */
+const AnimationWrapper: React.FC<Props> = (props) => {
+	return <ProfileCard {...props} />;
+};
+
+export default AnimationWrapper;
--- FrontEnd/code/src/screens/Home/index.tsx
+++ FrontEnd/code/src/screens/Home/index.tsx
@@ -1,14 +1,23 @@
 import ProfileCard from 'common/components/ProfileCard';
-import React from 'react';
+import React, { useState } from 'react';
+import OutsideClickHandler from 'react-outside-click-handler';
 
 import styles from './Home.module.sass';
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
+	const [open, setOpen] = useState(true);
 	return (
 		<main className={styles.main}>
-			<ProfileCard username={"SpongeBob"} />
+			<h3>
+				{open
+					? "Click outside to close me"
+					: "Click anywhere to open me"}
+			</h3>
+			<OutsideClickHandler onOutsideClick={() => setOpen((o) => !o)}>
+				<ProfileCard username={"SpongeBob"} visible={open} />
+			</OutsideClickHandler>
 		</main>
 	);
 };
